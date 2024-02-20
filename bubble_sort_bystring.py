7e5f365834a61916103fd2b5440e442361a69713
Elina Makipova <82317623+Corazonn01@users.noreply.github.com>
2024-02-20 10:39:15 +0300

    Update bubble_sort_bystring.py

--- bubble_sort_bystring.py
+++ bubble_sort_bystring.py
@@ -1,17 +1,20 @@
-
 def bubbleSort(arr):
     n = len(arr)
     for i in range(n - 1):
         for j in range(0, n - i - 1):
-            if len(arr[j][0]) > len(arr[j + 1][0]): 
+            if arr[j][1] > arr[j + 1][1]:  # Compare lengths
                 arr[j], arr[j + 1] = arr[j + 1], arr[j]
 
 with open('input.txt', 'r') as file:
     line = file.readline().strip()
-    words = [(word.strip("'"), len(word.strip("'"))) for word in line.strip("[]").split(', ')]  
+    words = [(word.strip("'"), len(word.strip("'"))) for word in line.strip("[]").split(', ')]
 
 bubbleSort(words)
 
-print("That is a sorted array:")
-for word, length in words:
-    print(f"{word}: {length}")
+words_dict = {word[0]: word[1] for word in words}
+
+with open('output.txt', 'w') as file:
+    for word, length in words_dict.items():
+        file.write(f'{word}: {length}\n')
+
+print("Результаты записаны в файл output.txt.")
