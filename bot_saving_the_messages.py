c0b1b9b345127eed35a303a8786d495908c76337
Elina Makipova <82317623+Corazonn01@users.noreply.github.com>
2024-03-18 20:55:23 +0300

    bot_saving_the_messages.py

--- bot_saving_the_messages.py
+++ bot_saving_the_messages.py
@@ -4,31 +4,19 @@ from telebot.types import Message
 TOKEN = '7151573905:AAG2VvqD8fPIe0SWvTr7Yfs_IqcK_1eW-lw'
 bot = telebot.TeleBot(TOKEN)
 
-# new_list = []
+new_list = []
 
 
 @bot.message_handler(commands=['start'])
 def start(message: Message):
     bot.reply_to(message,
                  f'Привет! Пожалуйста, напишите что-нибудь. Каждое ваше сообщение будет сохранено в моей системе.')
-    # user_answer = message.text
-    # new_list.append(user_answer)
 
 
-@bot.message_handler(commands=['stats'])
-def stats(message: Message):
-    chat_id = message.chat.id
-    if chat_id not in stats:
-        return "Статистика чата пуста"
-    else:
-        total_messages = stats[chat_id]['total_messages']
-        unique_users = len(stats[chat_id]['users'])
-        bot.reply_to(message,
-                     f"Статистика чата:\nВсего сообщений: {total_messages}\nУникальных пользователей: {unique_users}")
+@bot.message_handler(func=lambda message: True)
+def save_message(message: Message):
+    new_list.append(message.text)
+    bot.reply_to(message, f'here you have a list with your messages inside the chat: {new_list}')
 
 
-# bot.polling()
-try:
-    bot.polling()
-except Exception as e:
-    print(e)
+bot.polling()
