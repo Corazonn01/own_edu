4d465b1c6c75bcef5063b9fa0c54bbd3827f751e
Elina Makipova <82317623+Corazonn01@users.noreply.github.com>
2024-03-21 12:07:25 +0300

    bot_saving_the_messages.py

--- bot_saving_the_messages.py
+++ bot_saving_the_messages.py
@@ -13,19 +13,22 @@ def start(message: Message):
                  f'Привет! Пожалуйста, напишите что-нибудь. Каждое ваше сообщение будет сохранено в моей системе.')
 
 
-@bot.message_handler(func=lambda message: True)
-def save_message(message: Message):
-    new_list.append(message.text)
-    bot.reply_to(message, f'here you have a list with your messages inside the chat: {new_list}')
+# @bot.message_handler(func=lambda message: True)
+# def save_message(message: Message):
+#     new_list.append(message.text)
+#     bot.reply_to(message, f'here you have a list with your messages inside the chat: {new_list}')
 
 
 @bot.message_handler(func=lambda message: True)
 def storing(message: Message):
     with open('members.csv', 'a', newline='') as csvfile:
-        reader = csv.reader(csvfile)
-        reader.writerow([message.chat.id, message.from_user.id, message.text])
+        w = csv.writer(csvfile)
+        w.writerow([message.chat.id, message.from_user.id, message.text])
+        bot.send_message(message.chat.id, "your message was stored in csv file")
+    print(f"Message saved: {message.text}")
 
-        return f"Message saved: {message.text}"
+bot.polling()
+
 
 
 
